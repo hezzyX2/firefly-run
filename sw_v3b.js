8c106c56f2778769d42d1a98d2b179ccff3798a6
hezzyX2 <policicchio01@allegheny.edu>
2025-10-24 14:50:45 -0400

    Delete sw_v3b.js

--- sw_v3b.js
+++
@@ -1,5 +0,0 @@
-const CACHE='ff-pro-v3b';
-const ASSETS=['./','./index_v3.html?v=3','./style_v3.css?v=3','./game_v3.js?v=3','./manifest_v3.webmanifest','./favicon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))));self.clients.claim()});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
