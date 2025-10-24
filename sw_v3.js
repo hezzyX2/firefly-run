b5447b33c06635476c6dea24e173a4b8ef7842e5
hezzyX2 <policicchio01@allegheny.edu>
2025-10-23 23:10:46 -0400

    Delete sw_v3.js

--- sw_v3.js
+++
@@ -1,5 +0,0 @@
-const CACHE='ff-pro-v3';
-const ASSETS=['./','./index_v3.html','./style_v3.css','./game_v3.js','./manifest_v3.webmanifest','./favicon.svg'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k)))))});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
